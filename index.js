687aa404bd58c0066ec4cc30b0e9c2685b52bd75
ikticlh <122721105+ikticlh@users.noreply.github.com>
2023-01-16 00:50:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,4 +21,32 @@ function addOperator(op) {
   }
 }
 
+function calculate() {
+        if (firstOperand !== "" && operator !== "" && secondOperand !== "") {
+          switch (operator) {
+            case "+":
+              result = parseFloat(firstOperand) + parseFloat(secondOperand);
+              break;
+            case "-":
+              result = parseFloat(firstOperand) - parseFloat(secondOperand);
+              break;
+            case "*":
+              result = parseFloat(firstOperand) * parseFloat(secondOperand);
+              break;
+            case "/":
+              result = parseFloat(firstOperand) / parseFloat(secondOperand);
+              break;
+          }
+          display.value = result;
+        }
+      }
+
+
+function clearDisplay() {
+        firstOperand = "";
+        operator = "";
+        secondOperand = "";
+        display.value = "";
+      }
+
 
